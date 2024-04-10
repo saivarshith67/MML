f0e9f90dd565221cd518d5a248638ca1384248a5
saivarshith67 <saivarshith.coder@gmail.com>
2024-04-10 14:24:53 +0530

    Refactored `Regression` class method to function for linear regression line calculation
    * Moved the `linear_regression_line` method from `Regression` class to a standalone function
    * Removed the `__init__` method from the `Regression` class as it was not being used
    * Made necessary changes in the `main.py` to call the `linear_regression_line` function directly
    
    The functionality of the code remains the same.

--- 1_regression_and_correlation/regression/main.py
+++ 1_regression_and_correlation/regression/main.py
@@ -1,7 +1,6 @@
 import regression as rg
 
 if __name__ == '__main__':
-    x = rg.Regression()
     data_x = [1, 3, 4, 6, 8, 9, 11, 14]
     data_y = [1, 2, 4, 4, 5, 7, 8, 9]
-    x.linear_regression_line(data_x, data_y)
+    rg.linear_regression_line(data_x, data_y)
--- 1_regression_and_correlation/regression/regression.py
+++ 1_regression_and_correlation/regression/regression.py
@@ -1,34 +1,31 @@
 import numpy as np
 
-class Regression:
-    def __init__(self) -> None:
-        pass
+
+def linear_regression_line(data_x : list, data_y : list) -> None:
+    sum_of_prod = 0
+    sum_x = 0
+    sum_y = 0
+    sum_of_squares = 0
+    n = len(data_x)
     
-    def linear_regression_line(self, data_x : list, data_y : list) -> None:
-        self.sum_of_prod = 0
-        self.sum_x = 0
-        self.sum_y = 0
-        self.sum_of_squares = 0
-        self.n = len(data_x)
+    for i in range(n):
+        sum_of_prod += (data_x[i] * data_y[i])
+        sum_of_squares +=  ((data_x[i]) ** 2)
+        sum_x += data_x[i]
+        sum_y += data_y[i]
         
-        for i in range(self.n):
-            self.sum_of_prod += (data_x[i] * data_y[i])
-            self.sum_of_squares +=  ((data_x[i]) ** 2)
-            self.sum_x += data_x[i]
-            self.sum_y += data_y[i]
-            
-        """
-        linear regression
-        sum(y) = n*a + b*sum(x)
-        sum(xy) = a*sum(x) + b*n*sum(x^2)
-        
-        now we will solve it using numpy.linalg.solve that we have to create a matrix  A and B where A is [[self.n, self.sum_x],[self.sum_x, self.sum_of_squares]] and B is [[self.sum_y],[self.sum_of_prod]]
-        """
-        
-        self.A = np.array([[self.n, self.sum_x], [self.sum_x, self.sum_of_squares]])
-        self.B = np.array([[self.sum_y],[self.sum_of_prod]])
-        self.X = np.linalg.solve(self.A, self.B)
-        self.constant_term = float(self.X[0]) #constant
-        self.coefficient_of_x = float(self.X[1]) #coefficient of x
-        
-        print(f"Regression line: y = {self.constant_term} + {self.coefficient_of_x}*x")
+    """
+    linear regression
+    sum(y) = n*a + b*sum(x)
+    sum(xy) = a*sum(x) + b*n*sum(x^2)
+    
+    now we will solve it using numpy.linalg.solve that we have to create a matrix  A and B where A is [[n, sum_x],[sum_x, sum_of_squares]] and B is [[sum_y],[sum_of_prod]]
+    """
+    
+    A = np.array([[n, sum_x], [sum_x, sum_of_squares]])
+    B = np.array([[sum_y],[sum_of_prod]])
+    X = np.linalg.solve(A, B)
+    constant_term = float(X[0]) #constant
+    coefficient_of_x = float(X[1]) #coefficient of x
+    
+    print(f"Regression line: y = {constant_term} + {coefficient_of_x}*x")
